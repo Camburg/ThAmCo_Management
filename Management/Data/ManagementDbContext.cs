fb7bd71527706f3f8dc915e7ea55d424e3f91e67
CAMERON MACKENZIE <S6027509@live.tees.ac.uk>
2020-11-19 15:42:23 +0000

    Added DBContext for Purchase Requests

---
+++ Management/Data/ManagementDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Management.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Management.Data
+{
+    public class ManagementDbContext : DbContext
+    {
+        public DbSet<PurchaseRequest> PurchaseRequests { get; set; }
+
+        public ManagementDbContext(DbContextOptions<ManagementDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Management/Models/PurchaseRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Management.Models
+{
+    public class PurchaseRequest
+    {
+        public Guid Id { get; set; }
+        public string ItemName { get; set; }
+        public int Cost { get; set; }
+        public bool Accepted { get; set; }
+    }
+}
